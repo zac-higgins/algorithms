95880a848e04df6af9b66e3bba1701dfa53efa79
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-03-19 16:22:57 -0600

    added eating_cookies solution code from lecture. Completed recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,11 +3,19 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
-
+  # set a variable to hold the current highest amount
+  batches = []
+  for item, amount in recipe.items():
+    if ingredients.get(item) is None:
+      return 0
+    else:
+      possible_batches = ingredients.get(item) // amount
+      batches.append(possible_batches)
+  batches.sort()
+  return batches[0]
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
+  # Change the entries of these dictionaries to test
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
